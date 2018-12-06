2f7508c45f1a223b5a096d6afd3bf7e5ce479f54
sunyifu <syf516>
2018-12-06 09:59:45 +0800

    876

--- src/main/_273_DeleteNodeinaLinkedList.java
+++ src/main/_273_DeleteNodeinaLinkedList.java
@@ -1,5 +1,8 @@
 package main;
 
+
+import main.domian.ListNode;
+
 /**
  *
  */
@@ -12,56 +15,26 @@ public class _273_DeleteNodeinaLinkedList {
      * main.ListNode(int x) { val = x; }
      * }
      */
-    static  ListNode head = new ListNode(4).setNext(
-            (new ListNode(1)).setNext(
-                    (new ListNode(5).setNext(
-                            (new ListNode(4))
-                    )
-                    )
-            ));
     public static void deleteNode(ListNode node) {
-            node.val = node.next.val;
-            node.next = node.next.next;
+        node.val = node.next.val;
+        node.next = node.next.next;
     }
 
     public static void main(String[] args) {
-        printVal(head);
+        ListNode head = new ListNode(4).setNext(
+                (new ListNode(1)).setNext(
+                        (new ListNode(5).setNext(
+                                (new ListNode(4))
+                        )
+                        )
+                ));
+
+        ListNode.printVal(head);
         deleteNode(head.next.next);
         System.out.println();
-        printVal(head);
-    }
-
-    public static void printVal(ListNode node) {
-        System.out.println(node.val);
-        if (node.next != null)
-            printVal(node.next);
+        ListNode.printVal(head);
     }
 
 
 }
 
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode(int x) {
-        val = x;
-    }
-
-    public int getVal() {
-        return val;
-    }
-
-    public void setVal(int val) {
-        this.val = val;
-    }
-
-    public ListNode getNext() {
-        return next;
-    }
-
-    public ListNode setNext(ListNode next) {
-        this.next = next;
-        return this;
-    }
-}
