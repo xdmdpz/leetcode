bd661ec2d9eaa969a1c73673e4d94e0dfc4b4201
sunyifu <syf516>
2018-12-25 11:25:55 +0800

    例行更新

--- src/main/_huawei.java
+++ src/main/_huawei.java
@@ -1,26 +1,50 @@
 package main;
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
- * 判断牌型
- * 五张牌,每张牌由牌大小和花色组成,牌大小2~10、J、Q、K、A,牌花色为红桃、黑桃、
+ * 五张牌，每张牌由牌大小和花色组成，牌大小2~10、J、Q、K、A，牌花色为红桃、黑桃、梅花、方块四种花色之一。 判断牌型:
+ * 牌型1，同花顺：同一花色的顺子，如红桃2红桃3红桃4红桃5红桃6。
+ * 牌型2，四条：四张相同数字 + 单张，如红桃A黑桃A梅花A方块A + 黑桃K。
+ * 牌型3，葫芦：三张相同数字 + 一对，如红桃5黑桃5梅花5 + 方块9梅花9。
+ * 牌型4，同花：同一花色，如方块3方块7方块10方块J方块Q。
+ * 牌型5，顺子：花色不一样的顺子，如红桃2黑桃3红桃4红桃5方块6。
+ * 牌型6，三条：三张相同 + 两张单。
+ * 牌型7，其他。
+ * 说明：
+ * 1）五张牌里不会出现牌大小和花色完全相同的牌。
+ * 2）前面的牌型比后面的牌型大，如同花顺比四条大，依次类推。
  */
 public class _huawei {
-    int[] Hs = new int[13];
-    int[] Ss = new int[13];
-    int[] Cs = new int[13];
-    int[] Ds = new int[13];
-    int[] nums = new int[13];
+    ArrayList<String> colorLsit = new ArrayList<>();
+    HashSet<String> colorSet = new HashSet<>();
+    HashSet<String> numSet = new HashSet<>();
+    ArrayList<String> numList = new ArrayList<>();
+    static long begin, end;
 
     public static void main(String[] args) {
         //H红桃S黑桃C梅花D方片
-        String[] strings = {"2H", "3S", "4C", "5D", "6A"};
+        List<String> stringList = Arrays.asList("2 H", "3 S", "4 C", "5 H", "6 H");
+        _huawei huawei = new _huawei();
 
-        System.out.println(new _huawei().fun(strings));
+        begin();
+        System.out.println(huawei.fun(stringList));
+        end();
     }
 
-    public int fun(String[] strings) {
+    private static void begin() {
+        begin = System.currentTimeMillis();
+        System.out.println("开始：" + begin);
+    }
+
+    private static void end() {
+        end = System.currentTimeMillis();
+        System.out.println("结束：" + end);
+        System.out.println("耗时" + (end - begin));
+    }
+
+
+    public int fun(List<String> strings) {
         init(strings);
         if (is同花顺())
             return 1;
@@ -42,89 +66,49 @@ public class _huawei {
     }
 
     public boolean is葫芦() {
-        return max(nums) == 3 && is对(nums);
+        return numSet.size() == 2 &&
+                (!numList.get(0).equals(numList.get(1)) ||
+                        !numList.get(numList.size() - 1).equals(numList.get(numList.size() - 2)));
     }
 
     public boolean is三条() {
-        return max(nums) == 3 && is单(nums);
+        return numSet.size() == 3;
     }
 
     public boolean is同花() {
-        return sum(Hs) == 5 || sum(Ss) == 5 || sum(Cs) == 5 || sum(Ds) == 5;
+        return colorSet.size() == 1;
     }
 
     public boolean is顺子() {
-        for (int i = 0; i < 8; i++) {
-            if (sum(nums, i) == 5)
-                return true;
+        for (int i = 0; i < numList.size() - 1; i++) {
+            if (Integer.parseInt(numList.get(i + 1)) - 1 == Integer.parseInt(numList.get(i))) {
+                if (i == numList.size() - 2) {
+                    return true;
+                }
+            } else {
+                break;
+            }
         }
         return false;
     }
 
     public boolean is四条() {
-        return max(nums) == 4;
+        return numSet.size() == 2;
     }
 
-    public int sum(int[] ints) {
-        return Arrays.stream(ints).sum();
-    }
 
-    public int max(int[] ints) {
-        return Arrays.stream(ints).max().getAsInt();
-    }
+    public void init(List<String> strings) {
 
-    public boolean is对(int[] ints) {
-        return Arrays.stream(ints).filter(i -> i == 2).count() > 0;
-    }
+        for (int i = 0; i < 5; i++) {
+            String num = strings.get(i).substring(0, 1);
+            numList.add(num);
+            numSet.add(num);
+            String color = strings.get(i).substring(2);
+            colorLsit.add(color);
+            colorSet.add(color);
 
-    public boolean is单(int[] ints) {
-        return Arrays.stream(ints).filter(i -> i == 1).count() > 0;
-    }
-
-    public int sum(int[] ints, int begin) {
-        int[] ints1 = new int[5];
-        System.arraycopy(ints, begin, ints1, 0, 5);
-        return Arrays.stream(ints1).filter(i -> i == 1).sum();
-    }
-
-    public void init(String[] strings) {
-
-        for (String str : strings) {
-            String num = str.substring(0, 1);
-            int n;
-            String color = str.substring(1);
-            switch (num.toUpperCase()) {
-                case "J":
-                    n = 11 - 2;
-                    break;
-                case "Q":
-                    n = 12 - 2;
-                    break;
-                case "K":
-                    n = 13 - 2;
-                    break;
-                case "A":
-                    n = 14 - 2;
-                    break;
-                default:
-                    n = Integer.parseInt(num) - 2;
-            }
-            nums[n]++;
-            switch (color.toUpperCase()) {
-                case "H":
-                    Hs[n]++;
-                    break;
-                case "S":
-                    Ss[n]++;
-                    break;
-                case "C":
-                    Cs[n]++;
-                    break;
-                case "D":
-                    Ds[n]++;
-                    break;
-            }
         }
+        Collections.sort(numList);
 
     }
 }
